42661e1f2de10fda4a226049b9c482673aa61fb3
jeff15113 <itsmejeffery@gmail.com>
2019-04-25 19:06:45 -0400

    Python game

--- src/adv.py
+++ src/adv.py
@@ -1,5 +1,5 @@
 from room import Room
-
+from player import Player
 # Declare all the rooms
 
 room = {
@@ -38,6 +38,7 @@ room['treasure'].s_to = room['narrow']
 #
 
 # Make a new player object that is currently in the 'outside' room.
+Player = Player("Player", "outside")
 
 # Write a loop that:
 #
@@ -49,3 +50,9 @@ room['treasure'].s_to = room['narrow']
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
+
+while True:
+    userinput = input("->")
+
+    if (userinput & 'eat'):
+        Player.hunger = 100
--- src/player.py
+++ src/player.py
@@ -1,2 +1,16 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+
+class Player:
+    def __init__(self, name, currentroom, hunger=0):
+        self.name = name
+        self.currentroom = currentroom
+        self.inventory = []
+        self.hunger = hunger
+
+    def move_to(self, direction):
+        pass
+
+    def add_inventory(self, item):
+        self.inventory.append(item)
